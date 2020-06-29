0e092753c0fd39bcc2b62bf8689848b3b218e08f
IgorOFC <igorsci@gmail.com>
2020-06-29 11:44:38 -0300

    Updating code

--- scripts/options.js
+++ scripts/options.js
@@ -1,37 +1,73 @@
 window.onload = function () {
-  document.querySelector('#fb').textContent = chrome.i18n.getMessage('fb_title')
-  document.querySelector('#left').textContent = chrome.i18n.getMessage('left')
-  document.querySelector('#middle').textContent = chrome.i18n.getMessage('middle')
-  document.querySelector('#right').textContent = chrome.i18n.getMessage('right')
-  document.querySelector('#mode_seek_middle').textContent = chrome.i18n.getMessage('mode_seek_middle')
-  document.querySelector('#mode_volume').textContent = chrome.i18n.getMessage('mode_volume')
-  document.querySelector('#mode_seek_all').textContent = chrome.i18n.getMessage('mode_seek_all')
-  document.querySelector('#mode_everything').textContent = chrome.i18n.getMessage('mode_everything')
-  document.querySelector('#mode').textContent = chrome.i18n.getMessage('mode_title')
-  document.querySelector('#shortcut').textContent = chrome.i18n.getMessage('shortcut')
+  function saveChanges(option, value) {
+    chrome.storage.local.set({
+      [option]: value,
+    });
+  }
+  const pipCheckbox = document.querySelector("#pip");
+  const shortcutCheckbox = document.querySelector("#shortcut");
+  document.querySelector("#fb").textContent = chrome.i18n.getMessage(
+    "fb_title"
+  );
+  document.querySelector("#left").textContent = chrome.i18n.getMessage("left");
+  document.querySelector("#middle").textContent = chrome.i18n.getMessage(
+    "middle"
+  );
+  document.querySelector("#right").textContent = chrome.i18n.getMessage(
+    "right"
+  );
+  document.querySelector(
+    "#mode_seek_middle"
+  ).textContent = chrome.i18n.getMessage("mode_seek_middle");
+  document.querySelector("#mode_volume").textContent = chrome.i18n.getMessage(
+    "mode_volume"
+  );
+  document.querySelector("#mode_seek_all").textContent = chrome.i18n.getMessage(
+    "mode_seek_all"
+  );
+  document.querySelector(
+    "#mode_everything"
+  ).textContent = chrome.i18n.getMessage("mode_everything");
+  document.querySelector("#mode").textContent = chrome.i18n.getMessage(
+    "mode_title"
+  );
+  shortcutCheckbox.textContent = chrome.i18n.getMessage("shortcut");
+  pipCheckbox.textContent = chrome.i18n.getMessage("pip");
 
-  for (const input of document.querySelectorAll('[name]')) {
-    input.addEventListener('blur', function (e) {
-      const name = input.name
-      let saveValue
-      if (e.target.value === 'shortcut') {
-        saveValue = e.target.checked
-      } else {
-        saveValue = e.target.value
+  for (const checkbox of document.querySelectorAll("input[type=checkbox]")) {
+    checkbox.addEventListener("click", (e) => {
+      if (checkbox.id == "pip") {
+        // Disable other PIP related options if user disables it
+        if (!e.target.checked) {
+          shortcutCheckbox.disabled = true;
+        } else {
+          shortcutCheckbox.disabled = false;
+        }
       }
-      chrome.storage.local.set({
-        [name]: saveValue
-      })
-    })
+    });
+  }
+
+  for (const input of document.querySelectorAll("input")) {
+    input.addEventListener("blur", (e) => {
+      let value = e.target.value;
+      if (e.target.type == "checkbox") value = e.target.checked;
+      console.log(e.target.id, value);
+      saveChanges(e.target.id, value);
+    });
   }
 
   chrome.storage.local.get(function (options) {
-    if (options.mode === undefined) options.mode = 'mode_everything'
-    if (options.shortcut === undefined) options.shortcut = true
-    document.querySelector("[name='left'").value = options.left || 5
-    document.querySelector("[name='middle'").value = options.middle || 2
-    document.querySelector("[name='right'").value = options.right || 10
-    document.querySelector('#shortcut').checked = options.shortcut
-    document.querySelector('#' + options.mode).checked = true
-  })
-}
+    if (options.mode === undefined) options.mode = "mode_everything";
+    if (options.shortcut === undefined) options.shortcut = true;
+    if (options.pip === undefined) options.pip = true;
+    document.querySelector("[name='left'").value = options.left || 5;
+    document.querySelector("[name='middle'").value = options.middle || 2;
+    document.querySelector("[name='right'").value = options.right || 10;
+    pipCheckbox.checked = options.pip;
+    shortcutCheckbox.checked = options.shortcut;
+    if (!pipCheckbox.checked) {
+      shortcutCheckbox.disabled = true;
+    }
+    document.querySelector("#" + options.mode).checked = true;
+  });
+};
